d205edd3a99fecfd14ced801e9d30530aaa6b3b0
Hans Han <haniceboy@gmail.com>
2014-05-14 07:59:56 +0800

    1. add config for mysql 2. add the support for pydev.

--- meishi_server/meishi_server/settings.py
+++ meishi_server/meishi_server/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'meishi_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'WiFiAdvert',                      # Or path to database file if using sqlite3.
+        'NAME': 'sharemeishi',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'hanshan',
         'PASSWORD': '801112',
